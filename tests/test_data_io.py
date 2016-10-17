52871be0cff5633dfd2bacc620d365089bc3c13d
Talbert, Colin <talbertc@usgs.gov>
2016-10-17 14:48:02 -0600

    Finished setting up Test structure and first test

--- tests/test_data_io.py
+++ tests/test_data_io.py
@@ -11,6 +11,7 @@ import pymdwizard
 def test_read_csv():
     filepath = "tests/data/iris.csv"
     df = pymdwizard.core.data_io.read_csv(filepath)
+    print df.shape
     assert df.shape == (150, 5)
     assert list(df.columns) == [u'sepal_length', u'sepal_width',
                                 u'petal_length', u'petal_width',
