199193a5385c76afccf4db1aabdf65ba31832b49
Talbert, Colin <talbertc@usgs.gov>
2016-11-15 07:57:27 -0700

    update taxonomy tests

--- tests/test_taxonomy.py
+++ tests/test_taxonomy.py
@@ -6,19 +6,24 @@ import pytest
 import pandas as pd
 
 import pymdwizard
+from pymdwizard.core import taxonomy
 
 
 def test_search_common_name():
-    df = pymdwizard.core.taxonomy.search_by_common_name("brown bear")
+    df = taxonomy.search_by_common_name("brown bear")
     assert list(df.columns) == ['commonName', 'language', 'tsn']
 
-    results = pymdwizard.core.taxonomy.search_by_common_name("brown bear",
-                                                            as_dataframe=False)
+    results = taxonomy.search_by_common_name("brown bear", as_dataframe=False)
     assert list(results[0].keys()) == ['commonName', 'language', 'tsn']
+
+    taxonomy.pd = None
+    results = taxonomy.search_by_common_name("brown bear", as_dataframe=True)
+    assert list(results[0].keys()) == ['commonName', 'language', 'tsn']
+    taxonomy.pd = pd
 
 
 def test_search_scientific_name():
-    df = pymdwizard.core.taxonomy.search_by_scientific_name("gulo gulo")
+    df = taxonomy.search_by_scientific_name("gulo gulo")
     assert list(df.columns) == ['tsn', 'author', 'combinedName', 'kingdom',
                                 'unitInd1', 'unitInd2', 'unitInd3', 'unitInd4',
                                 'unitName1', 'unitName2', 'unitName3',
@@ -32,10 +37,45 @@ def test_search_scientific_name():
                                        'unitName2', 'unitName3', 'unitName4']
 
 
+def test_full_hierarchy_from_tsn():
+    df = taxonomy.get_full_hierarchy_from_tsn(180694)
+    assert list(df.columns) == ['tsn', 'author', 'parentName', 'parentTsn',
+                                'rankName', 'taxonName']
+
+    results = taxonomy.get_full_hierarchy_from_tsn(180694, as_dataframe=False)
+    assert results[0]['taxonName'] == 'Animalia'
+
+    df = taxonomy.get_full_hierarchy_from_tsn(180694, include_children=False)
+    assert df[df.rankName == 'Species'].empty
+
+    df = taxonomy.get_full_hierarchy_from_tsn(180694, include_children=True)
+    assert not df[df.rankName == 'Species'].empty
+
+
 def test_get_common_names_tsn():
-    df = pymdwizard.core.taxonomy.get_common_names_tsn(202385)
+    df = taxonomy.get_common_names_tsn(202385)
     assert list(df.columns) == ['commonName', 'language', 'tsn']
 
-    results = pymdwizard.core.taxonomy.get_common_names_tsn(202385,
-                                                           as_dataframe=False)
+    results = taxonomy.get_common_names_tsn(202385, as_dataframe=False)
     assert list(results[0].keys()) == ['commonName', 'language', 'tsn']
+
+
+def test_to_lower():
+    lower = taxonomy._to_lower(['Hello', 'WORLD'])
+    assert lower == ['hello', 'world']
+
+
+def test_gen_fgdc_taxonomy():
+    fgdc_taxonomy = taxonomy.gen_fgdc_taxonomy(tsns=[180694])
+    assert fgdc_taxonomy.tag == 'taxoncl'
+    assert fgdc_taxonomy.getchildren()[1].text == 'Animalia'
+
+    # Add a plant, so that the top level is Domain==Eukaryota
+    fgdc_taxonomy = taxonomy.gen_fgdc_taxonomy(tsns=[180694, 183437])
+    assert fgdc_taxonomy.getchildren()[1].text == 'Eukaryota'
+
+    # Add a bacteria, so that the top level is Domain==Life
+    fgdc_taxonomy = taxonomy.gen_fgdc_taxonomy(tsns=[180694, 183437, 951930])
+    assert fgdc_taxonomy.getchildren()[1].text == 'Life'
+
+
