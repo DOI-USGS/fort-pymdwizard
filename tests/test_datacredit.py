80e8faaed8c97e778b94f9a65228024e0e6c0d14
mhannon-usgs <mhannon@usgs.gov>
2017-01-25 09:53:02 -0700

    tests for data credit widget

---
+++ tests/test_datacredit.py
@@ -0,0 +1,34 @@
+from __future__ import print_function
+
+import sys
+sys.path.append(r"../..")
+
+import sys
+from pytestqt import qtbot
+from lxml import etree
+
+from PyQt5.QtWidgets import QWidget, QPlainTextEdit
+
+from pymdwizard.gui import DataCredit
+
+def test_datacredit__from_xml(qtbot):
+    widget = DataCredit.DataCredit()
+    qtbot.addWidget(widget)
+
+    test_record_fname = "tests/data/GenericFGDCTemplate_FGDCtemp.xml"
+    test_record = etree.parse(test_record_fname)
+    data_credit = test_record.xpath("idinfo/datacred")[0]
+
+    widget._from_xml(data_credit)
+    assert widget.findChild(QPlainTextEdit, "fgdc_datacred").toPlainText() == 'Data set Credits MH'
+
+def test_datacredit__to_xml(qtbot):
+    widget = DataCredit.DataCredit()
+    qtbot.addWidget(widget)
+
+    #test_record_fname = "C:/Users/mhannon/dev_mdwizard/pymdwizard/tests/data/Onshore_Industrial_Wind_Turbine_Locations_for_the_United_States_through_July2013.xml"
+    #test_record = etree.parse(test_record_fname)
+    #use_const = test_record.xpath("idinfo/useconst")[0]
+
+    widget._to_xml#(use_const)
+    assert widget.findChild(QPlainTextEdit, "fgdc_datacred").toPlainText() == ""
