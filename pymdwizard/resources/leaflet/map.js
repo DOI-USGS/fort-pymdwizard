976dc7747fff04cc5e008a2d0b7dce3eaccb687d
Colin Talbert <talbertc@usgs.gov>
2017-03-14 16:27:58 -0600

    Remove folium dependency

--- pymdwizard/resources/leaflet/map.js
+++ pymdwizard/resources/leaflet/map.js
@@ -1,20 +1,29 @@
-var southWest = L.latLng(-90, -180);
-var northEast = L.latLng(90, 180);
-var bounds = L.latLngBounds(southWest, northEast);
+var east = east_var;
+var west = west_var;
+var north = north_var;
+var south = south_var;
+
+var h_center = (east + west) / 2;
+var v_center = (north + south) / 2;
+
+var southWest = new L.latLng(south, west);
+var northEast = new L.latLng(north, east);
+var bounds = new L.latLngBounds(southWest, northEast);
 
 
 
 
 
-var map = L.map(
-    'map',
-    {center: [45.5236,-115.675],
-        zoom: 10,
-        maxBounds: bounds,
-        layers: [],
-        worldCopyJump: false,
-        crs: L.CRS.EPSG3857
-    });
+var map = new L.map(
+    'map'
+    // {center: [v_center, h_center],
+    //     zoom: 10,
+    //     maxBounds: bounds,
+    //     layers: [],
+    //     worldCopyJump: true,
+    //     crs: L.CRS.EPSG3857
+    // }
+    );
 
 
 
@@ -30,52 +39,50 @@ var tile_layer = L.tileLayer(
     }
 ).addTo(map);
 
-var e_marker = new L.marker([45.5,-115.6],{
+var e_marker = new L.marker([v_center, east],{
     draggable: true
 }).addTo(map);
 
-var w_marker = new L.marker([45.5,-115.7],{
+var w_marker = new L.marker([v_center, west],{
     draggable: true
 }).addTo(map);
 
-var n_marker = new L.marker([45.6,-115.65],{
+var n_marker = new L.marker([north, h_center],{
     draggable: true
 }).addTo(map);
 
-var s_marker = new L.marker([45.4,-115.65],{
+
+var s_marker = new L.marker([south, h_center],{
     draggable: true
 }).addTo(map);
 
-// marker.on("drag", function(e) {
-//     var marker = e.target;
-//     var position = marker.getLatLng();
-//     map.panTo(new L.LatLng(position.lat, position.lng));
-// });
 
 
-// var marker2 = L.marker(map.getCenter()).addTo(map);
-// marker2.bindPopup("Hello World  2!").openPopup();
-//
-if(typeof MainWindow != 'undefined') {
-    var onMapMove = function() { MainWindow.on_e_move(e_marker.getLatLng().lat, e_marker.getLatLng().lng) };
-    e_marker.on('move', onMapMove);
-    onMapMove();
+var rect = new L.rectangle(bounds, {color: 'blue', weight: 1}).addTo(map);
+map.fitBounds(bounds);
+
+south = south-1;
+s_marker.update();
+
+if(typeof Spdom != 'undefined') {
+    var onEMove = function () {
+        Spdom.on_e_move(e_marker.getLatLng().lat, e_marker.getLatLng().lng)
+    };
+    e_marker.on('move', onEMove);
+
+    var onWMove = function () {
+        Spdom.on_w_move(w_marker.getLatLng().lat, w_marker.getLatLng().lng)
+    };
+    w_marker.on('move', onWMove);
+
+    var onNMove = function () {
+        Spdom.on_n_move(n_marker.getLatLng().lat, n_marker.getLatLng().lng)
+    };
+    n_marker.on('move', onNMove);
+
+    var onSMove = function () {
+        Spdom.on_s_move(s_marker.getLatLng().lat, s_marker.getLatLng().lng)
+    };
+    s_marker.on('move', onSMove);
 }
 
-if(typeof MainWindow != 'undefined') {
-    var onMapMove = function() { MainWindow.onMapMove(w_marker.getLatLng().lat, w_marker.getLatLng().lng) };
-    w_marker.on('move', onMapMove);
-    onMapMove();
-}
-
-if(typeof MainWindow != 'undefined') {
-    var onMapMove = function() { MainWindow.onMapMove(e_marker.getLatLng().lat, e_marker.getLatLng().lng) };
-    e_marker.on('move', onMapMove);
-    onMapMove();
-}
-
-if(typeof MainWindow != 'undefined') {
-    var onMapMove = function() { MainWindow.onMapMove(e_marker.getLatLng().lat, e_marker.getLatLng().lng) };
-    e_marker.on('move', onMapMove);
-    onMapMove();
-}
