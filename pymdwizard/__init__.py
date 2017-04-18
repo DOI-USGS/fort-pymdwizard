de240a5cff21e269eb47b66c52912c5de3a2c709
Colin Talbert <talbertc@usgs.gov>
2017-04-18 11:17:54 -0600

    clean up testing

--- pymdwizard/__init__.py
+++ pymdwizard/__init__.py
@@ -1,4 +1,9 @@
 from __future__ import absolute_import
 
-from pymdwizard import gui
-from pymdwizard import core
+
+try:
+    import gui
+    import core
+except:
+    from pymdwizard import gui
+    from pymdwizard import core
