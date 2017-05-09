95b891be122e1d35576e724734010960c9b4cfa5
Colin Talbert <talbertc@usgs.gov>
2017-05-09 12:41:37 -0600

    add in attrdomv and rdom names for correct xpath

--- pymdwizard/gui/ui_files/UI_udom.py
+++ pymdwizard/gui/ui_files/UI_udom.py
@@ -8,18 +8,18 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-class Ui_udom_widget(object):
-    def setupUi(self, udom_widget):
-        udom_widget.setObjectName("udom_widget")
-        udom_widget.resize(171, 442)
+class Ui_fgdc_attrdomv(object):
+    def setupUi(self, fgdc_attrdomv):
+        fgdc_attrdomv.setObjectName("fgdc_attrdomv")
+        fgdc_attrdomv.resize(426, 442)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Ignored, QtWidgets.QSizePolicy.Preferred)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(udom_widget.sizePolicy().hasHeightForWidth())
-        udom_widget.setSizePolicy(sizePolicy)
-        self.verticalLayout = QtWidgets.QVBoxLayout(udom_widget)
+        sizePolicy.setHeightForWidth(fgdc_attrdomv.sizePolicy().hasHeightForWidth())
+        fgdc_attrdomv.setSizePolicy(sizePolicy)
+        self.verticalLayout = QtWidgets.QVBoxLayout(fgdc_attrdomv)
         self.verticalLayout.setObjectName("verticalLayout")
-        self.label = QtWidgets.QLabel(udom_widget)
+        self.label = QtWidgets.QLabel(fgdc_attrdomv)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Ignored, QtWidgets.QSizePolicy.Preferred)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
@@ -28,7 +28,7 @@ class Ui_udom_widget(object):
         self.label.setWordWrap(True)
         self.label.setObjectName("label")
         self.verticalLayout.addWidget(self.label)
-        self.fgdc_udom = QtWidgets.QPlainTextEdit(udom_widget)
+        self.fgdc_udom = QtWidgets.QPlainTextEdit(fgdc_attrdomv)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Ignored, QtWidgets.QSizePolicy.Expanding)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
@@ -37,11 +37,11 @@ class Ui_udom_widget(object):
         self.fgdc_udom.setObjectName("fgdc_udom")
         self.verticalLayout.addWidget(self.fgdc_udom)
 
-        self.retranslateUi(udom_widget)
-        QtCore.QMetaObject.connectSlotsByName(udom_widget)
+        self.retranslateUi(fgdc_attrdomv)
+        QtCore.QMetaObject.connectSlotsByName(fgdc_attrdomv)
 
-    def retranslateUi(self, udom_widget):
+    def retranslateUi(self, fgdc_attrdomv):
         _translate = QtCore.QCoreApplication.translate
-        udom_widget.setWindowTitle(_translate("udom_widget", "Form"))
-        self.label.setText(_translate("udom_widget", "Enter a Description of the Values Recorded in the Field"))
+        fgdc_attrdomv.setWindowTitle(_translate("fgdc_attrdomv", "Form"))
+        self.label.setText(_translate("fgdc_attrdomv", "Enter a Description of the Values Recorded in the Field"))
 
