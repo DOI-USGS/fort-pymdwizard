ae5a52d03f3597af4015d106d540d2277a695b70
Talbert <talbertc@usgs.gov>
2017-09-19 14:43:36 -0600

    Add DOI lookup for items in datacite

--- pymdwizard/core/datacite.py
+++ pymdwizard/core/datacite.py
@@ -1,4 +1,5 @@
 
+import requests
 import json
 
 try:
@@ -26,6 +27,8 @@ def clean_doi(doi):
         return doi.replace('https://doi.org/', '')
     elif doi.startswith('doi.org/'):
         return doi.replace('doi.org/', '')
+    elif doi.startswith('http://dx.doi.org'):
+        return doi.replace('http://dx.doi.org/', '')
     else:
         return doi
 
@@ -34,8 +37,23 @@ def get_doi_citation(doi):
     doi = clean_doi(doi)
     try:
         cite_data = json.loads(cn.content_negotiation(ids=doi, format = "citeproc-json"))
+        cite_data['geoform'] = 'publication'
+        cite_data['pubplace'] = 'n/a'
     except:
-        return None
+        try:
+            endpoint = 'https://api.datacite.org/works'
+            response = requests.get(endpoint + '/' + doi, verify=False)
+            cite_data = json.loads(response.text)['data']['attributes']
+            cite_data['publisher'] = cite_data['container-title']
+            cite_data['URL'] = 'https://doi.org/{}'.format(cite_data['doi'])
+            if cite_data['resource-type-id'] == 'dataset':
+                cite_data['container-title'] = None
+                cite_data['pubplace'] = 'https://www.sciencebase.gov'
+            else:
+                cite_data['geoform'] = 'publication'
+                cite_data['pubplace'] = 'n/a'
+        except:
+            return None
 
     citeinfo = XMLNode(tag='citeinfo')
     for author in cite_data['author']:
@@ -43,28 +61,31 @@ def get_doi_citation(doi):
 
     #
     if 'published-online' in cite_data:
-        pubdate_parts = text=pubonline=cite_data['published-online']['date-parts'][0]
+        pubdate_parts = cite_data['published-online']['date-parts'][0]
     elif 'published-print' in cite_data:
-        pubdate_parts = text=pubonline=cite_data['published-print']['date-parts'][0]
-
+        pubdate_parts = cite_data['published-print']['date-parts'][0]
+    elif 'published' in cite_data:
+        pubdate_parts = [cite_data['published'], ]
 
     pubdate_str = ''.join(['{:02d}'.format(part) for part in pubdate_parts])
     pubdate = XMLNode(tag='pubdate', parent_node=citeinfo, text=pubdate_str)
 
     title = XMLNode(tag='title', parent_node=citeinfo, text=cite_data['title'])
-    geoform = XMLNode(tag='geoform', parent_node=citeinfo, text='publication')
-    serinfo = XMLNode(tag='serinfo', parent_node=citeinfo)
-    sername = XMLNode(tag='sername', parent_node=citeinfo.serinfo, text=cite_data['container-title'])
+    geoform = XMLNode(tag='geoform', parent_node=citeinfo, text=cite_data['geoform'])
 
-    if 'volumne' in cite_data:
-        issue_str = 'vol.' + ' ' + str(cite_data['volume']) + ', issue ' + cite_data['issue'] + ', ppg. ' + cite_data['page']
-        issue = XMLNode(tag='issue', parent_node=citeinfo.serinfo, text=issue_str)
-    if 'issue' in cite_data:
-        issue_str = 'issue ' + cite_data['issue'] + ', ppg. ' + cite_data['page']
-        issue = XMLNode(tag='issue', parent_node=citeinfo.serinfo, text=issue_str)
+    if cite_data['container-title'] is not None:
+        serinfo = XMLNode(tag='serinfo', parent_node=citeinfo)
+        sername = XMLNode(tag='sername', parent_node=citeinfo.serinfo, text=cite_data['container-title'])
+
+        if 'volumne' in cite_data:
+            issue_str = 'vol.' + ' ' + str(cite_data['volume']) + ', issue ' + cite_data['issue'] + ', ppg. ' + cite_data['page']
+            issue = XMLNode(tag='issue', parent_node=citeinfo.serinfo, text=issue_str)
+        if 'issue' in cite_data:
+            issue_str = 'issue ' + cite_data['issue'] + ', ppg. ' + cite_data['page']
+            issue = XMLNode(tag='issue', parent_node=citeinfo.serinfo, text=issue_str)
 
     pubinfo = XMLNode(tag='pubinfo', parent_node=citeinfo)
-    pubplace = XMLNode(tag='pubplace', parent_node=citeinfo.pubinfo, text='n/a')
+    pubplace = XMLNode(tag='pubplace', parent_node=citeinfo.pubinfo, text=cite_data['pubplace'])
     publish = XMLNode(tag='publish', parent_node=citeinfo.pubinfo, text=cite_data['publisher'])
     onlink = XMLNode(tag='onlink', parent_node=citeinfo, text=cite_data['URL'])
 
