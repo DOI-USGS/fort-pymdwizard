90d243bed612514498c01b9aadcd7a4cd179a670
Colin Talbert <talbertc@usgs.gov>
2025-03-25 08:50:48 -0600

    Add logic for skipping FID insert

--- pymdwizard/core/data_io.py
+++ pymdwizard/core/data_io.py
@@ -146,7 +146,8 @@ def read_shp(fname):
 
     df = df[[c for c in df.columns if c != "geometry"]]
     df.insert(0, "Shape", c.schema["geometry"])
-    # df.insert(0, "FID", range(df.shape[0]))
+    if not "FID" in df.columns:
+        df.insert(0, "FID", range(df.shape[0]))
     return df
 
 
