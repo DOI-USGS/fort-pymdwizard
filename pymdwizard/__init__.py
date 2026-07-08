e23e941c58f17d67ed9902f90cf2909790ad92be
Tamar Norkin <tnorkin@usgs.gov>
2026-07-08 15:22:28 -0600

    Clean up docstring headers and complete truncated PURPOSE statement

--- pymdwizard/__init__.py
+++ pymdwizard/__init__.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
 """
-The MetadataWizard(pymdwizard) software was developed by the
-U.S. Geological Survey Fort Collins Science Center.
-See: https://github.com/usgs/fort-pymdwizard for current project source code
-See: https://usgs.github.io/fort-pymdwizard/ for current user documentation
-See: https://github.com/usgs/fort-pymdwizard/tree/master/examples
-    for examples of use in other scripts
+The MetadataWizard (pymdwizard) software was developed by the U.S. Geological
+Survey Fort Collins Science Center.
 
 License:            CC0 1.0 Universal
                     https://creativecommons.org/publicdomain/zero/1.0/
--- pymdwizard/gui/ui_files/spellinghighlighter.py
+++ pymdwizard/gui/ui_files/spellinghighlighter.py
@@ -1,19 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
 """
-The MetadataWizard(pymdwizard) software was developed by the
-U.S. Geological Survey Fort Collins Science Center.
-See: https://github.com/usgs/fort-pymdwizard for current project source code
-See: https://usgs.github.io/fort-pymdwizard/ for current user documentation
-See: https://github.com/usgs/fort-pymdwizard/tree/master/examples
-    for examples of use in other scripts
+The MetadataWizard (pymdwizard) software was developed by the U.S. Geological
+Survey Fort Collins Science Center.
 
 License:            CC0 1.0 Universal
                     https://creativecommons.org/publicdomain/zero/1.0/
 
 PURPOSE
 ------------------------------------------------------------------------------
-Contains a
+Contains a custom PyQt5 syntax highlighter for real-time spell checking in 
+text widgets
 
 
 SCRIPT DEPENDENCIES
@@ -25,26 +22,9 @@ SCRIPT DEPENDENCIES
     inter-package dependencies
 
 
-U.S. GEOLOGICAL SURVEY DISCLAIMER
+NOTES
 ------------------------------------------------------------------------------
-This software has been approved for release by the U.S. Geological Survey (USGS).
-Although the software has been subjected to rigorous review,
-the USGS reserves the right to update the software as needed pursuant to
-further analysis and review. No warranty, expressed or implied, is made by
-the USGS or the U.S. Government as to the functionality of the software and
-related material nor shall the fact of release constitute any such warranty.
-Furthermore, the software is released on condition that neither the USGS nor
-the U.S. Government shall be held liable for any damages resulting from
-its authorized or unauthorized use.
-
-Any use of trade, product or firm names is for descriptive purposes only and
-does not imply endorsement by the U.S. Geological Survey.
-
-Although this information product, for the most part, is in the public domain,
-it also contains copyrighted material as noted in the text. Permission to
-reproduce copyrighted items for other than personal use must be secured from
-the copyright owner.
-------------------------------------------------------------------------------
+None
 """
 
 from PyQt5 import QtCore, QtGui
